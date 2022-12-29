ac612d99bd6042d177f42655c95039ba13ea6c35
uamadasun <uchennaamadasun@gmail.com>
2022-12-28 21:38:36 -0800

    her wallet initial project setup, testing coinbase API for current btc price

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from "axios";
+
 
 function App() {
+  //function to get us price of btc
+  const [price, setPrice] = useState(0);
+
+  const getPrice = () => {
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      
+      //runs if API is called successfully
+      .then((res) => {
+        setPrice(res.data.data.amount);
+      })
+
+      //runs should API call fail
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  //hook, lets us run the function at a specific time
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Current Bitcoin Price</h1>
+      <h3>${price}</h3>
     </div>
   );
 }
